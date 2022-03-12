a1cafcb25b53c7a85c588beb53f4e512b23baafc
rout2203 <Thomas.Roux-Croteau@Usherbrooke.ca>
2022-03-12 00:00:35 -0500

    added FPS count

--- Projet STR/Assets/Scripts/TestsManager.cs
+++ Projet STR/Assets/Scripts/TestsManager.cs
@@ -1,11 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 /// <summary>
 /// Classe chargée de vérifier que les différentes contraites temps réel sont bien respectées
 /// </summary>
 public class TestsManager : MonoBehaviour
 {
-    
+	public TextMeshProUGUI FpsText;
+
+	private float pollingTime = 1f;
+	private float temps;
+	private int nbImages;
+
+	void Update()
+	{
+		// affichage du taux d'images par seconde
+		temps += Time.deltaTime;
+		nbImages++;
+
+		if (temps >= pollingTime)
+        {
+			int frameRate = Mathf.RoundToInt(nbImages / temps);
+			FpsText.text = frameRate.ToString() + " SPF";
+			temps -= pollingTime;
+			nbImages = 0;
+        }
+	}
+
 }
