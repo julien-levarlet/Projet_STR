98ad4916fb287dd99d48c532489aeba6859c0941
julien <jlevarle@gmail.com>
2022-03-16 18:16:35 -0400

    Mise en place d'un tableau de stockage des fps

--- Projet STR/Assets/Scripts/TestsManager.cs
+++ Projet STR/Assets/Scripts/TestsManager.cs
@@ -1,33 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.Serialization;
 
 /// <summary>
 /// Classe chargée de vérifier que les différentes contraites temps réel sont bien respectées
+/// Cette est un singleton, il ne peut exister qu'une seule fois dans la scene
+/// Pour enregistrer les valeurs sur un fichier il faut appuyer sur S
 /// </summary>
 public class TestsManager : MonoBehaviour
 {
-	public TextMeshProUGUI FpsText;
+	// affichage des fps
+	public TextMeshProUGUI fpsText;
 
-	private float pollingTime = 1f;
-	private float temps;
-	private int nbImages;
+	// stockage des valeurs des contraintes 
+	private const int NbValuesStored = 1000;
+	private int _fpsIndex;
+	private float[] _fpsValues; // tableaux des valeurs de temps entre deux images mesurés (parcours de façon cyclique)
+	
+	// instance du singleton
+	private static TestsManager _instance;
 
-	void Update()
+	private void Start()
 	{
+		_fpsValues = new float[1000];
+		_fpsIndex = -1;
 		Application.targetFrameRate = 61;
-		// affichage du taux d'images par seconde
-		temps += Time.deltaTime;
-		nbImages++;
-
-		if (temps >= pollingTime)
-        {
-			int frameRate = Mathf.RoundToInt(nbImages / temps);
-			FpsText.text = frameRate.ToString() + " FPS";
-			temps -= pollingTime;
-			nbImages = 0;
-        }
 	}
 
+	void Awake()  // à l'instanciation 
+	{
+		if (_instance == null) // si on est la première instance on l'affecte à _instance
+		{
+			_instance = this;
+			return;
+		}
+		// si une instance est déjà existante, cet objet n'est pas valide, il faut le détruire
+		Destroy(gameObject);
+	}
+	
+	void Update()
+	{
+		_fpsIndex = (_fpsIndex + 1) % NbValuesStored;
+		_fpsValues[_fpsIndex] = Time.deltaTime;
+		if (Input.GetKey(KeyCode.S)) // appuie sur S pour enregistrer les constantes
+		{
+			SaveValues();
+		}
+	}
+
+	private void OnGUI()
+	{
+		fpsText.text = Mathf.RoundToInt(1/_fpsValues[_fpsIndex]) + "FPS"; // affichage du nb de fps
+	}
+
+	private static void SaveValues()
+	{
+		
+	}
 }
