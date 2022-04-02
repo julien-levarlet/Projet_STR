96fe9cb1c865fff6c35b6be8b77eec5c5cb63311
julien <jlevarle@gmail.com>
2022-04-02 14:37:03 -0400

    Mise en place de la scene d'entrainement

---
+++ Projet STR/Assets/Scripts/Neat/NeatPlayer.cs
@@ -0,0 +1,48 @@
+using SharpNeat.Phenomes;
+using UnityEngine;
+
+namespace Neat
+{
+    public class NeatPlayer : NeatAgent
+    {
+        protected override void Start()
+        {
+            base.Start();
+        }
+        
+        protected override float GetInputVertical()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override float GetInputHorizontal()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool AttackCondition()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void UpdateBlackBoxInputs(ISignalArray inputSignalArray)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void UseBlackBoxOutpts(ISignalArray outputSignalArray)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override float GetFitness()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void HandleIsActiveChanged(bool newIsActive)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
